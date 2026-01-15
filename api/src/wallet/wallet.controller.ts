786ab6203f0a85a11efb3f39a06c909a643411db
neves350 <martimneves350@gmail.com>
2026-01-15 13:47:42 +0000

    refactor(wallets): add api responses for endpoints

--- api/src/wallet/wallet.controller.ts
+++ api/src/wallet/wallet.controller.ts
@@ -10,6 +10,13 @@ import {
 } from '@nestjs/common'
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import {
+	ApiCreateResponses,
+	ApiDeleteResponses,
+	ApiFindAllResponses,
+	ApiFindOneResponses,
+	ApiUpdateByIdResponses,
+} from 'src/decorators/api-responses/wallet-responses.decorator'
 import { CurrentUser } from 'src/decorators/current-user.decorator'
 import { CreateWalletDto } from './dtos/create-wallet.dto'
 import { UpdateWalletDto } from './dtos/update-wallet.dto'
@@ -27,6 +34,7 @@ export class WalletController {
 		summary: 'Create a new wallet',
 		description: 'Creates a new wallet with name, type, curreny and balance.',
 	})
+	@ApiCreateResponses()
 	async create(@Body() createWalletDto: CreateWalletDto, @CurrentUser() user) {
 		const wallet = await this.walletService.create(createWalletDto, user.userId)
 
@@ -43,6 +51,7 @@ export class WalletController {
 		summary: 'Get all wallets',
 		description: 'Get all wallets from user.',
 	})
+	@ApiFindAllResponses()
 	async findAll(@CurrentUser() user) {
 		const wallets = await this.walletService.findAll(user.userId)
 
@@ -56,6 +65,7 @@ export class WalletController {
 		summary: 'Get wallet by id',
 		description: 'Get wallet for the user.',
 	})
+	@ApiFindOneResponses()
 	async findOne(@Param('id') id: string, @CurrentUser() user) {
 		const wallet = await this.walletService.findOne(id, user.sub)
 
@@ -69,6 +79,7 @@ export class WalletController {
 		summary: 'Update wallet by id',
 		description: 'Updates the wallet information.',
 	})
+	@ApiUpdateByIdResponses()
 	async updateById(
 		@Param('id') id: string,
 		@Body() updateWalletDto: UpdateWalletDto,
@@ -90,6 +101,7 @@ export class WalletController {
 		summary: 'Delete wallet by id',
 		description: 'Deletes the wallet information.',
 	})
+	@ApiDeleteResponses()
 	async delete(@Param('id') id: string, @CurrentUser() _user) {
 		return this.walletService.delete(id)
 	}
