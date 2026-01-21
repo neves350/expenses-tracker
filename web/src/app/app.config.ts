9609763df859c2d67b61a1a5077fc77a7121026d
neves350 <martimneves350@gmail.com>
2026-01-21 17:33:21 +0000

    feat(auth): register auth interceptor in app configuration

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,15 +1,16 @@
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import {
 	type ApplicationConfig,
 	provideBrowserGlobalErrorListeners,
 } from '@angular/core'
 import { provideRouter } from '@angular/router'
+import { authInterceptor } from '@core/interceptors/auth.interceptor'
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideBrowserGlobalErrorListeners(),
 		provideRouter(routes),
-		provideHttpClient(),
+		provideHttpClient(withInterceptors([authInterceptor])),
 	],
 }
