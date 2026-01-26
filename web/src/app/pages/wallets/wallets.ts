ca32cd0ed85c3345317ad1f05b16e46f3b0e2c8e
neves350 <martimneves350@gmail.com>
2026-01-26 20:08:34 +0000

    feat(wallets): add wallets page

---
+++ web/src/app/pages/wallets/wallets.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallets',
+  imports: [],
+  templateUrl: './wallets.html',
+  styleUrl: './wallets.css',
+})
+export class Wallets {
+
+}
