525350d4e08b4bb0a9214e16920438912171973c
neves350 <martimneves350@gmail.com>
2026-01-27 23:23:10 +0000

    9. refactor(layout): integrate sidebar with layout

--- web/src/app/shared/components/layout/layout.ts
+++ web/src/app/shared/components/layout/layout.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { Header } from '../header/header'
+import { Sidebar } from '../sidebar/sidebar'
+import { HlmSidebarImports } from '../ui/spartan/sidebar/src'
 
 @Component({
 	selector: 'app-layout',
-	imports: [Header, RouterOutlet],
+	imports: [Header, RouterOutlet, Sidebar, HlmSidebarImports],
 	templateUrl: './layout.html',
 	styleUrl: './layout.css',
 })
