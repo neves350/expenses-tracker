46a1dc16df25a49b9b0dedcf2dd5c143e224df9f
neves350 <martimneves350@gmail.com>
2026-01-30 13:02:53 +0000

    feat(dashboard): integrate all dashboard components

--- web/src/app/pages/dashboard/dashboard.ts
+++ web/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { DashboardCard } from '@/shared/components/dashboard/dashboard-card/dashboard-card'
+import { DashboardChart } from '@/shared/components/dashboard/dashboard-chart/dashboard-chart'
+import { DashboardTransactions } from '@/shared/components/dashboard/dashboard-transactions/dashboard-transactions'
+import { DashboardWallets } from '@/shared/components/dashboard/dashboard-wallets/dashboard-wallets'
 
 @Component({
 	selector: 'app-dashboard',
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [],
+	imports: [
+		DashboardCard,
+		DashboardChart,
+		DashboardWallets,
+		DashboardTransactions,
+	],
 	templateUrl: './dashboard.html',
 	styleUrl: './dashboard.css',
 })
