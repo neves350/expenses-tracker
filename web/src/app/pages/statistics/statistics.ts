15732301a3b6991b6393da9f1ec574a5518167a7
neves350 <martimneves350@gmail.com>
2026-02-23 13:22:49 +0000

    feat(statistic): connect period filter to statistics page

--- web/src/app/pages/statistics/statistics.ts
+++ web/src/app/pages/statistics/statistics.ts
@@ -1,21 +1,38 @@
 import { Component, inject } from '@angular/core'
+import type { StatisticsQueryParams } from '@core/api/statistics.interface'
 import { BankAccountsService } from '@core/services/bank-accounts.service'
+import { StatisticsService } from '@core/services/statistics.service'
 import { ChartPieIcon, LucideAngularModule } from 'lucide-angular'
 import { StatisticsFilter } from '@/shared/components/statistics/statistics-filter/statistics-filter'
+import { StatisticsSummary } from '@/shared/components/statistics/statistics-summary/statistics-summary'
 import { ZardCardComponent } from '@/shared/components/ui/card'
 
 @Component({
 	selector: 'app-statistics',
-	imports: [ZardCardComponent, LucideAngularModule, StatisticsFilter],
+	imports: [
+		ZardCardComponent,
+		LucideAngularModule,
+		StatisticsFilter,
+		StatisticsSummary,
+	],
 	templateUrl: './statistics.html',
 })
 export class Statistics {
 	private readonly bankAccountsService = inject(BankAccountsService)
+	private readonly statisticsService = inject(StatisticsService)
 
 	readonly accounts = this.bankAccountsService.bankAccounts
+	readonly hasData = this.statisticsService.hasData
 	readonly ChartPieIcon = ChartPieIcon
 
 	constructor() {
 		this.bankAccountsService.loadBankAccounts().subscribe()
+		this.statisticsService
+			.loadStatistics({ period: this.statisticsService.period() })
+			.subscribe()
+	}
+
+	onFilterChange(params: StatisticsQueryParams) {
+		this.statisticsService.loadStatistics(params).subscribe()
 	}
 }
